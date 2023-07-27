53f355684dd44079e39a9ad495cea435d34df7c8
anhf <anhf@xiaoma.cn>
2023-07-27 16:26:04 +0800

    feat: 支持获取数据

--- background/background.js
+++ background/background.js
@@ -1,5 +1,24 @@
-let color = '#3aa757';
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-})
+const regex1 = /api\/interface\/get/;
+const regex2 = /^(?!.*jiangniao).*$/;
+chrome.webRequest.onCompleted.addListener(
+  function (details) {
+    if (
+      details.method === "GET" &&
+      regex1.test(details.url) &&
+      regex2.test(details.url)
+    ) {
+      console.log("GET 请求 URL:", details.url);
+        fetch(`${details.url}&jiangniao`)
+          .then((response) => response.text())
+          .then((data) => {
+            // 接口中数据，待处理
+            console.log(data);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    }
+  },
+  { urls: ["<all_urls>"], types: ["xmlhttprequest"] },
+  []
+);
